5f3bf5831ba03c9fac1adc85c3d8b0a032f7aefc
cressid <kroe@mit.edu>
2013-11-19 23:31:10 -0500

    elcounter

--- circuit.js
+++ circuit.js
@@ -1,3 +1,10 @@
 var circuit = function(){
-
+	//counts the number of elements in the circuit from the parsed text
+	var elCounter = function(text){
+		//should parse the text into nice little tokens for us
+		parsed = parser.decomment(parser.line_extend(parser.split(text)))
+		
+	}
+	
+	return {elCounter:elCounter}
 }
