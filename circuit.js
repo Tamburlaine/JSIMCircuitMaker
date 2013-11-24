2a9a16a530cc04987378e1893bb48cb732c4747a
cressid <kroe@mit.edu>
2013-11-24 15:05:59 -0500

    with input box

--- circuit.js
+++ circuit.js
@@ -1,10 +1,16 @@
 var circuit = function(){
+	console.log("circuit is working!")
 	//counts the number of elements in the circuit from the parsed text
 	var elCounter = function(text){
 		//should parse the text into nice little tokens for us
 		parsed = parser.decomment(parser.line_extend(parser.split(text)))
+		console.log(parsed)
 		
+		return {parsed:parsed}
 	}
 	
+	elCounter("kipper")
+	
+	
 	return {elCounter:elCounter}
-}
+}()
