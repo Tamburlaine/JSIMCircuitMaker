a43ced97127f2dd9f6aeee2e56e2ac243f76a938
cressid <kroe@mit.edu>
2013-12-14 14:31:18 -0500

    event handler added

--- circuit.js
+++ circuit.js
@@ -1,6 +1,33 @@
 var circuit = function(){
 
+    function EventHandler(){
+        var handlers={};
+        //map event string to list of callbacks
+        function on(eventString, callback){
+            var cblist=handlers[eventString]
+            if(cblist===undefined){
+                cblist=[]
+                handlers[eventString]=cblist;
+            }
+            cblist.push(callback);
+            
+        }
+        function trigger(eventString,data){
+            var cblist=handlers[eventString];
+            if(cblist!== undefined){
+                for (var i=0;i<cblist.length;i+=1)
+                {
+                    cblist[i](data);
+                }
+            }
+        }
+            return{on:on, trigger: trigger}
+        }
+
 	var Model = function(Div){
+	
+		var eventHandler = EventHandler()
+		
 		
 		//counts the number of elements in the circuit from the parsed text
 		var elCounter = function(text){
@@ -16,12 +43,14 @@ var circuit = function(){
 	
 	var View = function(div, model){
 		
+		
 		//initializes the screen's display
 		var buildDisplay=function(){
+		
 			div.append("Circuit:");
 			
 			var circuitInp = $('<br><textarea class = "circuitInp" id = circuitInp width = 200px height = 100px>JSIM code goes here</textarea><br>');
-			var circuitButt = $('<button class = "circuitButt">Click!</button>');
+			var circuitButt = $('<button class = "circuitButt" onclick="test_fun()">Click!</button>');
 			
 			circuitButt.onclick=function(){
 				console.log("kipper butt");
@@ -39,7 +68,8 @@ var circuit = function(){
 			}
 		}
 
-		return {buildDisplay:buildDisplay}
+		return {buildDisplay:buildDisplay,
+				on:eventHandler.on}
 	}
 	
 	var setup = function (div) {
